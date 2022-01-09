97c40da34ca7f8ad27650706e5953abe40981b30
Christinchick <alexey@skyward-crimea.ru>
2022-01-09 16:09:41 +0300

    Add new

--- src/main/java/CarProject.java
+++ src/main/java/CarProject.java
@@ -1,13 +1,13 @@
-public class Car_Project {
+public class CarProject {
 
-    static class Cars {
+    // static class Cars {
 
         String NameModel;
         int Year;
         int VolumeEngine;
         int AverageSpeed;
 
-        public void Power() {
+        public void power() {
        if (VolumeEngine >= 2500) {
             System.out.println("Машина мощная " + NameModel + " можно ехать в путешествие");
             } else {
@@ -15,11 +15,11 @@ public class Car_Project {
                  }
         }
 
-        public void Way() {
+        public void way() {
             System.out.println(NameModel + " Доедет 1000км примерно за " + 1000 / AverageSpeed + " часов");
         }
 
-        public void Old(){
+        public void old(){
             if (Year > 2000) {
                 System.out.println("Машина " + NameModel + " еще походит");}
             else if (Year < 2000 && Year > 1980){
@@ -27,7 +27,7 @@ public class Car_Project {
             } else {
                 System.out.println("Поздравляем! У Вас " + NameModel + " ретро автомобиль");}
         }
-    }
+   // }
 }
 
 
--- src/main/java/MainCars.java
+++ src/main/java/MainCars.java
@@ -1,30 +1,30 @@
 public class MainCars {
     public static void main(String[] args) {
-        Car_Project.Cars Volvo = new Car_Project.Cars();
+        CarProject Volvo = new CarProject();
         Volvo.NameModel = "S80";
         Volvo.Year = 2001;
         Volvo.VolumeEngine = 2500;
         Volvo.AverageSpeed = 160;
 
-        Car_Project.Cars Toyota = new Car_Project.Cars();
+        CarProject Toyota = new CarProject();
         Toyota.NameModel = "Carina";
         Toyota.Year = 1989;
         Toyota.VolumeEngine = 1600;
         Toyota.AverageSpeed = 120;
 
-        Car_Project.Cars Ford = new Car_Project.Cars();
+        CarProject Ford = new CarProject();
         Ford.NameModel = "Mustang";
         Ford.Year = 1969;
         Ford.VolumeEngine = 3906;
         Ford.AverageSpeed = 180;
 
-        Toyota.Power();
-        Ford.Power();
-        Volvo.Way();
-        Toyota.Way();
-        Ford.Way();
-        Toyota.Old();
-        Volvo.Old();
-        Ford.Old();
+        Toyota.power();
+        Ford.power();
+        Volvo.way();
+        Toyota.way();
+        Ford.way();
+        Toyota.old();
+        Volvo.old();
+        Ford.old();
     }
 }
